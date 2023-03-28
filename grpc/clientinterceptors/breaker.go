e46d85f50e50fb5ed55a398172bc6e5aefaf973b
hangaoshuai <hangaoshuai2022@163.com>
2023-03-28 14:59:45 +0800

    add service break

--- grpc/clientinterceptors/breaker.go
+++ grpc/clientinterceptors/breaker.go
@@ -1,6 +1,7 @@
 package clientinterceptors
 
 import (
+	"context"
 	"google.golang.org/grpc"
 
 	"github.com/afex/hystrix-go/hystrix"
@@ -14,4 +15,18 @@ func BreakerUnary(cc hystrix.CommandConfig, cfgs ...ghystrix.HystrixCfg) grpc.Un
 
 func BreakerStream(cc hystrix.CommandConfig, cfgs ...ghystrix.HystrixCfg) grpc.StreamClientInterceptor {
 	return grpc_hystrix.StreamClientInterceptor(cc, cfgs...)
+}
+
+func BreakerUnaryByService(serviceName string, cc hystrix.CommandConfig) grpc.UnaryClientInterceptor {
+	hystrix.ConfigureCommand(serviceName, cc)
+	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		return hystrix.Do(serviceName, func() error { return invoker(ctx, method, req, reply, cc, opts...) }, nil)
+	}
+}
+
+func BreakerStreamByService(serviceName string, cc hystrix.CommandConfig) grpc.StreamClientInterceptor {
+	hystrix.ConfigureCommand(serviceName, cc)
+	return func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (cs grpc.ClientStream, err error) {
+		return cs, hystrix.Do(serviceName, func() error { cs, err = streamer(ctx, desc, cc, method, opts...); return err }, nil)
+	}
 }
