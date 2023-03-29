5bb5e8a9b4e56a72baa4d26f2df58e6d2c4229a4
hangaoshuai2022 <116149939+hangaoshuai2022@users.noreply.github.com>
2023-03-29 16:46:26 +0800

    fix ginx limiter middleware (#88)

--- ginx/limit.go
+++ ginx/limit.go
@@ -2,14 +2,13 @@ package ginx
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/time/rate"
 )
 
 func LimiterMiddleware(limit int) gin.HandlerFunc {
-	limiter := rate.NewLimiter(rate.Every(time.Second), limit)
+	limiter := rate.NewLimiter(rate.Limit(limit), limit)
 	return func(c *gin.Context) {
 		if !limiter.Allow() {
 			c.JSON(http.StatusTooManyRequests, Response{
