6b4203b029d598668c638c5054f981775f211a69
hangaoshuai <hangaoshuai2022@163.com>
2023-06-06 17:23:11 +0800

    support redis lock

--- distributedlock/redislock.go
+++ distributedlock/redislock.go
@@ -2,27 +2,130 @@ package distributedlock
 
 import (
 	"context"
-	"github.com/go-redis/redis/v8"
+	"errors"
+	"fmt"
 	"time"
+
+	"github.com/go-redis/redis/v8"
 )
 
 var (
 	defaultLockKey    = "dioRedisLockKey"
 	defaultExpiration = 10 * time.Second
+	defaultRetryTTL   = time.Second
+	//luaRefresh = redis.NewScript(`if redis.call("get", KEYS[1]) == ARGV[1] then return redis.call("expire", KEYS[1], ARGV[2]) else return 0 end`)
+	luaRelease = redis.NewScript(`if redis.call("get", KEYS[1]) == ARGV[1] then return redis.call("del", KEYS[1]) else return 0 end`)
+	luaPTTL    = redis.NewScript(`if redis.call("get", KEYS[1]) == ARGV[1] then return redis.call("pttl", KEYS[1]) else return -3 end`)
 )
 
-type redisLock struct {
-	client *redis.Client
+var (
+	// ErrNotObtained is returned when a lock cannot be obtained.
+	ErrNotObtained = errors.New("redislock: not obtained")
+
+	// ErrLockNotHeld is returned when trying to release an inactive lock.
+	ErrLockNotHeld = errors.New("redislock: lock not held")
+)
+
+type RedisLock struct {
+	client     *redis.Client
+	lockKey    string
+	expiration time.Duration
+	retryTTL   time.Duration
+	unlockCh   chan struct{}
 }
 
-func (rl *redisLock) Lock() {
+type Option func(redisLock *RedisLock)
+
+func WithLockKey(lockKey string) Option {
+	return func(redisLock *RedisLock) {
+		redisLock.lockKey = lockKey
+	}
+}
+
+func WithExpiration(expiration time.Duration) Option {
+	return func(redisLock *RedisLock) {
+		redisLock.expiration = expiration
+	}
+}
+
+func WithRetryTTL(retryTTL time.Duration) Option {
+	return func(redisLock *RedisLock) {
+		redisLock.retryTTL = retryTTL
+	}
+}
+
+func New(rclient *redis.Client, opts ...Option) *RedisLock {
+	redisLock := &RedisLock{
+		client:     rclient,
+		lockKey:    defaultLockKey,
+		expiration: defaultExpiration,
+		retryTTL:   defaultRetryTTL,
+	}
+	for _, opt := range opts {
+		opt(redisLock)
+	}
+	return redisLock
+}
+
+func (rl *RedisLock) Lock() {
 	lockValue, err := getLockValue()
 	if err != nil {
-		panic(err)
+		fmt.Printf("get lock value error %v\n", err)
+		return
 	}
 
-	ok, err := rl.client.SetNX(context.Background(), defaultLockKey, lockValue, defaultExpiration).Result()
-	if err != nil {
-		panic(err)
+	for {
+		ok, err := rl.client.SetNX(context.Background(), defaultLockKey, lockValue, defaultExpiration).Result()
+		if err != nil {
+			fmt.Printf("set lock error %v\n", err)
+			return
+		}
+		if ok {
+			fmt.Printf("get lock success %v time %v\n", lockValue, time.Now().String())
+			return
+		}
+		fmt.Printf("--------------get lock failed %v time %v---------------\n", lockValue, time.Now().String())
+		time.Sleep(defaultRetryTTL)
 	}
+}
+
+func (rl *RedisLock) Unlock() {
+	lockValue, err := getLockValue()
+	if err != nil {
+		fmt.Printf("get lock value error %v\n", err)
+	}
+	res, err := luaRelease.Run(context.Background(), rl.client, []string{defaultLockKey}, lockValue).Result()
+	if err == redis.Nil {
+		fmt.Printf("release error %v\n", ErrLockNotHeld)
+		return
+	} else if err != nil {
+		fmt.Printf("release error %v\n", err)
+		return
+	}
+
+	if i, ok := res.(int64); !ok || i != 1 {
+		fmt.Printf("release error res value type\n")
+		return
+	}
+	fmt.Printf("release lock %v at time %v\n", lockValue, time.Now().String())
+	return
+}
+
+// TTL returns the remaining time-to-live. Returns 0 if the lock has expired.
+func (rl *RedisLock) TTL() (time.Duration, error) {
+	lockValue, err := getLockValue()
+	if err != nil {
+		fmt.Printf("get lock value error %v\n", err)
+	}
+	res, err := luaPTTL.Run(context.Background(), rl.client, []string{defaultLockKey}, lockValue).Result()
+	if err == redis.Nil {
+		return 0, nil
+	} else if err != nil {
+		return 0, err
+	}
+
+	if num := res.(int64); num > 0 {
+		return time.Duration(num) * time.Millisecond, nil
+	}
+	return 0, nil
 }
--- distributedlock/redislock_test.go
+++ distributedlock/redislock_test.go
@@ -1,10 +1,41 @@
 package distributedlock
 
 import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"sync"
 	"testing"
+	"time"
 )
 
 func Test_redisLock_Lock(t *testing.T) {
-	rl := redisLock{}
-	rl.Lock()
+	redisCli := redis.NewClient(&redis.Options{
+		Addr:     "120.27.236.102:6379",
+		DB:       0,
+		Password: "123456",
+	})
+
+	err := redisCli.Ping(context.Background()).Err()
+	if err != nil {
+		t.Errorf("ping redis error %v", err)
+		return
+	}
+	rl := RedisLock{client: redisCli}
+	var wg sync.WaitGroup
+	for i := 1; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			rl.Lock()
+			defer wg.Done()
+			defer rl.Unlock()
+			for j := 0; j < 16; j++ {
+				ttl, err := rl.TTL()
+				va, _ := getLockValue()
+				fmt.Printf("err is %v, va is %v ttl is %v, time is %v\n", err, va, ttl, time.Now().String())
+				time.Sleep(time.Second)
+			}
+		}()
+	}
+	wg.Wait()
 }
