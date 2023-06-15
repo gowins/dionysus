bc9337d900efd5fa3475fde0fa2023c5c6f2c0ec
hangaoshuai <hangaoshuai2022@163.com>
2023-06-15 15:35:43 +0800

    fix config unit test

--- config/config_test.go
+++ config/config_test.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"github.com/gowins/dionysus/log"
 	"github.com/spf13/viper"
 	"testing"
@@ -9,13 +10,14 @@ import (
 
 func TestSetup(t *testing.T) {
 	log.Setup()
-	expectRes := []string{"4222", "g43g", "vwevw", "f2ff2"}
+	expectRes := []string{"4222", "g43g", "f2ff2", "vwevw"}
 	getRes := []string{}
 	configHandlers := []*WatchConfigHandler{
 		{
 			Key: "Redis.UserName",
 			Func: func(valueString string) error {
 				getRes = append(getRes, valueString)
+				fmt.Printf("UserName valueString is %v\n", valueString)
 				return nil
 			},
 		},
@@ -23,6 +25,7 @@ func TestSetup(t *testing.T) {
 			Key: "Mysql.DbName",
 			Func: func(valueString string) error {
 				getRes = append(getRes, valueString)
+				fmt.Printf("DbName valueString is %v\n", valueString)
 				panic("test panic")
 				return nil
 			},
@@ -31,14 +34,16 @@ func TestSetup(t *testing.T) {
 	Setup(configHandlers...)
 	viper.Set("Mysql.DbName", "4222")
 	viper.WriteConfig()
-	time.Sleep(time.Second * 20)
+	time.Sleep(time.Second * 2)
 	viper.Set("Redis.UserName", "g43g")
 	viper.WriteConfig()
-	time.Sleep(time.Second * 20)
+	time.Sleep(time.Second * 2)
 	viper.Set("Mysql.DbName", "f2ff2")
+	viper.WriteConfig()
+	time.Sleep(time.Second * 2)
 	viper.Set("Redis.UserName", "vwevw")
 	viper.WriteConfig()
-	time.Sleep(time.Second * 20)
+	time.Sleep(time.Second * 2)
 	for index, res := range expectRes {
 		if res != getRes[index] {
 			t.Errorf("want string %v, get string %v ", res, getRes[index])
